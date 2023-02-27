d04748f8316df39df6d89cd885a0518ac054eeb7
Bruno <bruno.moreira@meta.com.br>
2023-02-27 14:35:38 -0300

    week 2 - python exercises.

---
+++ pythonProject/algorithms.py
@@ -0,0 +1,16 @@
+print('Hello world!')
+
+celsius = 23
+
+kelvin = celsius + 273
+fahrenheit = 1.8 * celsius + 32
+
+# print(kelvin)
+# print(fahrenheit)
+
+x = 2
+y = 3
+z = -4
+q = (x**y)/abs(-4)
+print(q)
+
