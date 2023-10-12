e0a15bf991a6854249eab5a8e2156133864b9b0e
Bruno <moreirasbr@gmail.com>
2023-10-12 12:01:13 -0300

    Roman To Integer exercise

--- exercises/week1/palindromeNumber.js
+++ exercises/week1/palindromeNumber.js
@@ -1,10 +1,6 @@
-function isPalindrome(x) {
-    // Transform the number into a string because we can't reverse a number
+function isPalindromeTS(x) {
     var stringNumber = x.toString();
-    // Reverse the string and join it back together to make it a number again
     var reversedStringNumber = stringNumber.split('').reverse().join('');
-    // Compare the original number with the reversed number
-    return x === parseInt(reversedStringNumber);
+    return stringNumber === reversedStringNumber;
 }
-
-console.log(isPalindrome(111));
+console.log(isPalindromeTS(1201));
