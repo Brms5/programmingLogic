91dab16ccdc0f50efe5936c90919a6146ce130a1
Bruno <moreirasbr@gmail.com>
2023-10-29 18:50:41 -0300

    generics classes and interfaces

---
+++ object_oriented_p/object-oriented-programming/src/main/java/com/generics/GenericsClass.java
@@ -0,0 +1,29 @@
+package com.generics;
+
+public class GenericsClass<K, V> {
+
+    private K key;
+    private V value;
+
+    public GenericsClass(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return this.key;
+    }
+
+    public V getValue() {
+        return this.value;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+    
+}
